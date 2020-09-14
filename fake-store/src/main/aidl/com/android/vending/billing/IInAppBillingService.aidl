fffa00a85c6157ab8f1d005444a2d9219aef5c51
Marvin W <git@larma.de>
2020-09-15 00:37:36 +0200

    Complete IInAppBillingService interface

--- fake-store/src/main/aidl/com/android/vending/billing/IInAppBillingService.aidl
+++ fake-store/src/main/aidl/com/android/vending/billing/IInAppBillingService.aidl
@@ -185,5 +185,19 @@ interface IInAppBillingService {
     Bundle getBuyIntentToReplaceSkus(int apiVersion, String packageName,
         in List<String> oldSkus, String newSku, String type, String developerPayload) = 6;
 
-    Bundle queryPurchases(int apiVersion, String packageName, String type, String continuationToken, in Bundle extras) = 10;
+    Bundle getBuyIntentV6(int apiVersion, String packageName, String sku, String type, String developerPayload, in Bundle extras) = 7;
+
+    Bundle getPurchasesV6(int apiVersion, String packageName, String type, String continuationToken, in Bundle extras) = 8;
+
+    int isBillingSupportedV7(int apiVersion, String packageName, String type, in Bundle extras) = 9;
+
+    Bundle getPurchasesV9(int apiVersion, String packageName, String type, String continuationToken, in Bundle extras) = 10;
+
+    Bundle consumePurchaseV9(int apiVersion, String packageName, String purchaseToken, in Bundle extras) = 11;
+
+    Bundle getPriceChangeConfirmationIntent(int apiVersion, String packageName, String sku, String type, in Bundle extras) = 800;
+
+    Bundle getSkuDetailsV10(int apiVersion, String packageName, String type, in Bundle skuBundle, in Bundle extras) = 900;
+
+    Bundle acknowledgePurchase(int apiVersion, String packageName, String purchaseToken, in Bundle extras) = 901;
 }
--- fake-store/src/main/java/com/android/vending/billing/InAppBillingService.java
+++ fake-store/src/main/java/com/android/vending/billing/InAppBillingService.java
@@ -20,42 +20,27 @@ public class InAppBillingService extends Service {
     private final IInAppBillingService.Stub mInAppBillingService = new IInAppBillingService.Stub() {
         @Override
         public int isBillingSupported(int apiVersion, String packageName, String type) throws RemoteException {
-            Log.d(TAG, "isBillingSupported(" + apiVersion + ", " + packageName + ", " + type + ")");
-            return 0;
+            return isBillingSupportedV7(apiVersion, packageName, type, new Bundle());
         }
 
         @Override
         public Bundle getSkuDetails(int apiVersion, String packageName, String type, Bundle skusBundle) throws RemoteException {
-            Log.d(TAG, "getSkuDetails(" + apiVersion + ", " + packageName + ", " + type + ")");
-            Bundle data = new Bundle();
-            data.putInt("RESPONSE_CODE", 0);
-            data.putStringArrayList("DETAILS_LIST", new ArrayList<String>());
-            return data;
+            return getSkuDetailsV10(apiVersion, packageName, type, skusBundle, new Bundle());
         }
 
         @Override
         public Bundle getBuyIntent(int apiVersion, String packageName, String sku, String type, String developerPayload) throws RemoteException {
-            Log.d(TAG, "getBuyIntent(" + apiVersion + ", " + packageName + ", " + sku + ", " + type + ", " + developerPayload + ")");
-            Bundle data = new Bundle();
-            data.putInt("RESPONSE_CODE", 4);
-            return data;
+            return getBuyIntentV6(apiVersion, packageName, sku, type, developerPayload, new Bundle());
         }
 
         @Override
         public Bundle getPurchases(int apiVersion, String packageName, String type, String continuationToken) throws RemoteException {
-            Log.d(TAG, "getPurchases(" + apiVersion + ", " + packageName + ", " + type + ", " + continuationToken + ")");
-            Bundle data = new Bundle();
-            data.putInt("RESPONSE_CODE", 0);
-            data.putStringArrayList("INAPP_PURCHASE_ITEM_LIST", new ArrayList<String>());
-            data.putStringArrayList("INAPP_PURCHASE_DATA_LIST", new ArrayList<String>());
-            data.putStringArrayList("INAPP_DATA_SIGNATURE_LIST", new ArrayList<String>());
-            return data;
+            return getPurchasesV6(apiVersion, packageName, type, continuationToken, new Bundle());
         }
 
         @Override
         public int consumePurchase(int apiVersion, String packageName, String purchaseToken) throws RemoteException {
-            Log.d(TAG, "consumePurchase(" + apiVersion + ", " + packageName + ", " + purchaseToken + ")");
-            return 8;
+            return consumePurchaseV9(apiVersion, packageName, purchaseToken, new Bundle()).getInt("RESPONSE_CODE", 8);
         }
 
         @Override
@@ -67,13 +52,65 @@ public class InAppBillingService extends Service {
         }
 
         @Override
-        public Bundle queryPurchases(int apiVersion, String packageName, String type, String continuationToken, Bundle extras) throws RemoteException {
-            Log.d(TAG, "queryPurchases(" + apiVersion + ", " + packageName + ", " + type + ", " + continuationToken + ")");
+        public Bundle getBuyIntentV6(int apiVersion, String packageName, String sku, String type, String developerPayload, Bundle extras) throws RemoteException {
+            Log.d(TAG, "getBuyIntent(" + apiVersion + ", " + packageName + ", " + sku + ", " + type + ", " + developerPayload + ")");
+            Bundle data = new Bundle();
+            data.putInt("RESPONSE_CODE", 4);
+            return data;
+        }
+
+        @Override
+        public Bundle getPurchasesV6(int apiVersion, String packageName, String type, String continuationToken, Bundle extras) throws RemoteException {
+            return getPurchasesV9(apiVersion, packageName, type, continuationToken, extras);
+        }
+
+        @Override
+        public int isBillingSupportedV7(int apiVersion, String packageName, String type, Bundle extras) throws RemoteException {
+            Log.d(TAG, "isBillingSupported(" + apiVersion + ", " + packageName + ", " + type + ")");
+            return 0;
+        }
+
+        @Override
+        public Bundle getPurchasesV9(int apiVersion, String packageName, String type, String continuationToken, Bundle extras) throws RemoteException {
+            Log.d(TAG, "getPurchases(" + apiVersion + ", " + packageName + ", " + type + ", " + continuationToken + ")");
             Bundle data = new Bundle();
             data.putInt("RESPONSE_CODE", 0);
             data.putStringArrayList("INAPP_PURCHASE_ITEM_LIST", new ArrayList<String>());
             data.putStringArrayList("INAPP_PURCHASE_DATA_LIST", new ArrayList<String>());
             data.putStringArrayList("INAPP_DATA_SIGNATURE_LIST", new ArrayList<String>());
+            return data;
+        }
+
+        @Override
+        public Bundle consumePurchaseV9(int apiVersion, String packageName, String purchaseToken, Bundle extras) throws RemoteException {
+            Log.d(TAG, "consumePurchase(" + apiVersion + ", " + packageName + ", " + purchaseToken + ")");
+            Bundle data = new Bundle();
+            data.putInt("RESPONSE_CODE", 8);
+            return data;
+        }
+
+        @Override
+        public Bundle getPriceChangeConfirmationIntent(int apiVersion, String packageName, String sku, String type, Bundle extras) throws RemoteException {
+            Log.d(TAG, "getPriceChangeConfirmationIntent(" + apiVersion + ", " + packageName + ", " + sku + ", " + type + ")");
+            Bundle data = new Bundle();
+            data.putInt("RESPONSE_CODE", 4);
+            return data;
+        }
+
+        @Override
+        public Bundle getSkuDetailsV10(int apiVersion, String packageName, String type, Bundle skuBundle, Bundle extras) throws RemoteException {
+            Log.d(TAG, "getSkuDetails(" + apiVersion + ", " + packageName + ", " + type + ")");
+            Bundle data = new Bundle();
+            data.putInt("RESPONSE_CODE", 0);
+            data.putStringArrayList("DETAILS_LIST", new ArrayList<String>());
+            return data;
+        }
+
+        @Override
+        public Bundle acknowledgePurchase(int apiVersion, String packageName, String purchaseToken, Bundle extras) throws RemoteException {
+            Log.d(TAG, "acknowledgePurchase(" + apiVersion + ", " + packageName + ", " + purchaseToken + ")");
+            Bundle data = new Bundle();
+            data.putInt("RESPONSE_CODE", 8);
             return data;
         }
 
